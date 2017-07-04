3ca8c9408ae4d5c705ba4d1ed8891e18ccb2d18c
Rachael <rcoulter700@googlemail.com>
2017-07-04 14:51:54 +0100

    Can now upload a picture, need to predict photo

--- index.js
+++ index.js
@@ -1,28 +1,22 @@
 const express = require('express');
-const axios = require('axios');
 const file_upload = require('express-fileupload')
-
 const app = express();
 
-app.use(express.static('static'));
-app.use(express.static('public'));
 app.use(file_upload());
 
-app.post('/upload', function(req, res){
-    "use strict";
-    if(!req.files)
-        return res.status(400).send('No files were uploaded.');
+app.use(express.static('static'));
+
+app.post('/upload', function(req, res) {
     
-    // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file 
-    let sampleFile = req.files.sampleFile;
- 
-    // Use the mv() method to place the file somewhere on your server 
-    sampleFile.mv('/somewhere/on/your/server/filename.jpg', function(err) {
-        if (err)
-            return res.status(500).send(err);
- 
-    res.send('File uploaded!');
-  });
+  if (!req.files)
+    return res.status(400).send('No files were uploaded.');
+
+  // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
+  var sampleFile = req.files.sampleFile;
+
+  // Use the mv() method to place the file somewhere on your server
+  sampleFile.mv('public/'+ sampleFile.name);
+  res.send(sampleFile.name);
 });
 
-app.listen(3000);
+app.listen(8000);
